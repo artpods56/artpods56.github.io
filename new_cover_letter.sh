dd3847ed4fe7b762d16abb7ece9e05860a56545c
Artur Podsiadły <artpods56@gmail.com>
2025-09-20 12:51:30 +0200

    feat: add cover letter templates and generator script

---
+++ new_cover_letter.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Cover Letter Generator Script
+# Usage: ./new_cover_letter.sh <company_name> <language>
+# Language: en or pl
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <company_name> <language>"
+    echo "Language: en or pl"
+    echo "Example: $0 google en"
+    exit 1
+fi
+
+COMPANY_NAME="$1"
+LANGUAGE="$2"
+
+# Create directory name (lowercase, replace spaces with underscores)
+DIR_NAME="applications/cover_$(echo "$COMPANY_NAME" | tr '[:upper:]' '[:lower:]' | tr ' ' '_')"
+
+if [ -d "$DIR_NAME" ]; then
+    echo "Directory $DIR_NAME already exists!"
+    exit 1
+fi
+
+# Create new directory
+mkdir "$DIR_NAME"
+
+# Copy necessary files
+cp "coverletter/cover_${LANGUAGE}.cls" "$DIR_NAME/"
+cp "coverletter/template_${LANGUAGE}.tex" "$DIR_NAME/cover_letter.tex"
+cp "coverletter/signature.png" "$DIR_NAME/"
+cp -r "coverletter/OpenFonts" "$DIR_NAME/"
+
+echo "Cover letter template created in: $DIR_NAME"
+echo "Edit $DIR_NAME/cover_letter.tex to customize for $COMPANY_NAME"
+echo "Compile with: cd $DIR_NAME && lualatex cover_letter.tex"
